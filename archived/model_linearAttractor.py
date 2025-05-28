37ca9f7357f19efe83854c842a0c4ef821c4bcec
Xavier Chen <ec2478692@berkeley.edu>
2025-05-28 15:01:01 -0700

    Convolution Attractor

--- archived/model_linearAttractor.py
+++ archived/model_linearAttractor.py
@@ -11,18 +11,17 @@ class Model(torch.nn.Module):
         self.resnet_model = resnet18(weights=weights)
 
         self.model = torch.nn.Sequential(*(list(self.resnet_model.children())[:-2]))
-        self.avgpool = nn.AvgPool2d(kernel_size=2, stride=2, padding=0)
+        self.avgpool = nn.AvgPool2d(kernel_size=6, stride=1, padding=0)
         self.fc1 = nn.Linear(512, 256)
         self.fc2 = nn.Linear(256 ,num_classes)
-        self.attractor = ConvolutionAttractor(hidden_neurons=hidden_neurons, N=num_iter, dropout=dropout)
-        self.conv = nn.Conv2d(512, 512, kernel_size=3)
+        self.attractor = Attractor(hidden_neurons=hidden_neurons, N=num_iter, dropout=dropout)
 
     def forward(self, x):
         x = self.model(x)
         x = self.avgpool(x)
-        x = self.attractor(x)
-        x = self.conv(x)
         x = x.view(x.size(0), -1)  # Replaces torch.squeeze(x)
+        #x = torch.squeeze(x)
+        x = self.attractor(x)
         x = self.fc1(x)
         x = self.fc2(x)
 
@@ -37,34 +36,8 @@ class Model(torch.nn.Module):
         x = torch.flatten(x, 1)
         return x
 
-class ConvolutionAttractor(nn.Module):
-    # Input Shape: (batch_size, 512, 3, 3)
-    # Output Shape: (batch_size, 512, 3, 3)
-    def __init__(self, N=1, hidden_neurons=100, dropout=0.25):
-        super(ConvolutionAttractor, self).__init__()
-        
-        self.skip = nn.Conv2d(512, 512, kernel_size=1)
-        self.down = nn.Conv2d(512, 512, kernel_size=3)
-        self.up   = nn.Upsample(size=(3, 3))
-        self.activation = nn.Tanh()
-        self.N = N
-        self.dropout = nn.Dropout(dropout)
-    
-    def flow(self, x):
-        x_skip = self.skip(x)
-        x_hidden = self.up(self.down(x))
-        x = self.activation(x_skip + x_hidden)
-        return x
-    
-    def forward(self, x):
-        x = self.dropout(x)
-        for _ in range(self.N):
-            x = self.flow(x)
-        return x
 
 class Attractor(nn.Module):
-    # Input Shape: 512
-    # Output Shape: 512
     def __init__(self, N=1, hidden_neurons=100, dropout=0.25):
         super(Attractor, self).__init__()
         
