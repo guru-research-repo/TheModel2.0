23643554c0e31b50704b39f015b4670f3d8610e8
atahan <atahan@ucsd.edu>
2025-10-25 01:28:24 +0000

    gabor filters

--- salience_trans.py
+++ salience_trans.py
@@ -10,6 +10,8 @@ Each function returns an output with the same shape as its input. Batch support 
 
 """
 
+import os
+from pathlib import Path
 import random
 import cv2
 import torch
@@ -19,6 +21,7 @@ import torchvision.transforms.functional as TF
 import numpy as np
 import mediapipe as mp
 from trans import *
+from PIL import Image
 
 #######################################
 ########## Helper functions ###########
@@ -31,7 +34,7 @@ face_mesh = mp_face_mesh.FaceMesh(static_image_mode=True)
 # Determine facial landmarks
 def get_facial_features(image):
     assert isinstance(image, np.ndarray), f"Expected np array, got {type(image)}."
-        
+    
     h, w, _ = image.shape
     rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     results = face_mesh.process(rgb)
@@ -75,6 +78,8 @@ def get_facial_features(image):
 
 # Randomly sample salient points
 def sample_facial_feature_points_weighted(feature_dict, num_points=1):
+    
+    
     if "face_bbox" not in feature_dict:
         raise ValueError("Face bounding box is missing from features.")
 
@@ -145,6 +150,74 @@ class SaliencePipeline(torch.nn.Module):
         self.lp_true = logpolar
         self.crop = RandomCrop(n=n_crops, crop_size=img_size)
 
+        self.kernels = self.get_kernels()
+        self.num_kernels = len(self.kernels)
+
+    # Get kernels for gabor filters, each in different direction TODO: test out reducing the number of these!
+    def get_kernels(self):
+        kernels = []
+        size = (31,31)
+        lambd = [5.0,11.0]
+        sigma = [0.56*l for l in lambd]
+        psi = [0.0, np.pi/2] # without both it has 2 lines, more wavy
+        theta = [0.0,np.pi/4,2*np.pi/4,3*np.pi/4]#4*np.pi/4,5*np.pi/4,6*np.pi/4,7*np.pi/4] #could probably remove second half
+        gamma = 0.5
+
+        for l in range(len(lambd)):
+            for p in range(len(psi)):
+                for t in range(len(theta)):
+                    kernels.append(cv2.getGaborKernel(size, sigma[l], theta[t], lambd[l], gamma, psi[p]))
+            
+        return kernels
+
+
+    def sample_salience_points(self, img, center = None):
+        h, w, _ = img.shape
+        filtered = []#np.zeros((len(self.kernels), h, w))
+
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY).astype(np.float32)
+        out_img = TF.to_pil_image(img)
+        filename = f"out/img_proc_original.png"
+        out_img.save(filename)
+
+        # gaussian mask
+        if center is None:
+            center_x = w / 2 - 0.5
+            center_y = h / 2 - 0.5
+        else:
+            center_x, center_y = center
+
+        x = torch.arange(0, w, dtype=torch.float32)
+        y = torch.arange(0, h, dtype=torch.float32)
+        y_grid, x_grid = torch.meshgrid(y, x, indexing='ij')
+        gaussian_mask = torch.exp(-((x_grid - center_x)**2 + (y_grid - center_y)**2) / (2 * (w/6)**2))
+
+        weighted_img = gaussian_mask * img
+        weighted_img = (weighted_img - weighted_img.min()) / (weighted_img.max() - weighted_img.min())
+        
+        for i in range(self.num_kernels):
+            f = (cv2.filter2D(np.array(weighted_img), cv2.CV_32F, self.kernels[i]))
+            f = (f - np.mean(f)) / np.std(f)
+            filtered.append(f)
+            # out_img = TF.to_pil_image(filtered[i].astype(np.uint8))
+            # filename = f"out/img_proc_filtered-{i}.png"
+            # out_img.save(filename)
+
+        # seems to do worse
+        # mag = []
+        # for i in range(self.num_kernels//2):
+        #     mag.append(np.sqrt(filtered[i]**2 + filtered[i+self.num_kernels//2]**2))
+        # calculate variance
+        variance = torch.var(torch.from_numpy(np.array(filtered, dtype=np.float32)), dim=0)
+        variance = (variance - variance.min()) / (variance.max() - variance.min())
+        out_img = TF.to_pil_image(variance)
+        filename = f"out/img_proc_variance.png"
+        out_img.save(filename)
+
+        weights = variance.flatten(start_dim=-2, end_dim=-1)
+        features = torch.multinomial(weights, self.num_salient_points)
+        return torch.stack(torch.unravel_index(features, variance.shape), dim=-1)
+    
     def forward(self, img): 
         assert isinstance(img, torch.Tensor), f"Expected Tensor, got {type(img)}."
         
@@ -156,12 +229,7 @@ class SaliencePipeline(torch.nn.Module):
 
         # Loop through each identity in batch
         for b in range(B): 
-            features = get_facial_features(img_np[b])  
-            if features is None:
-                # if face not detected, randomly sample salient points from a square with corners (80,80) & (144,144)
-                salient_points = torch.rand((self.num_salient_points, 2),device=self.device)*(144-80)+80            
-            else: 
-                salient_points = torch.tensor(sample_facial_feature_points_weighted(features, num_points=self.num_salient_points),device=self.device)
+            salient_points = self.sample_salience_points(img_np[b])
 
             for salient_idx, center in enumerate(salient_points):
                 if self.type == 'train':
@@ -181,3 +249,22 @@ class SaliencePipeline(torch.nn.Module):
                 transformed_imgs[b,salient_idx] = transformed_img
                 
         return transformed_imgs
+    
+
+if __name__ == "__main__":
+    img_path = 'data/faces_cleaned/faces_cleaned/4_identities/test/EmmanuelMacron/183.jpg' # image or directory of images
+    
+    img_path = Path(img_path).expanduser()
+
+    if os.path.exists(img_path):
+        if os.path.isfile(img_path):
+            try:
+                image = Image.open(img_path).convert("RGB")
+            except Exception:
+                print('error')
+                exit(0)
+
+            tensor_img = TF.to_tensor(image).unsqueeze(0)
+            pipeline = SaliencePipeline('train')
+            pipeline(tensor_img)
+
